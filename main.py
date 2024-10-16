b61e61d617de51419d1500349f093e07f67741d0
Kalel Molano <contagiousmisfortune@gmail.com>
2024-10-16 06:52:40 -0500

    changes to the entire code

--- main.py
+++ main.py
@@ -10,15 +10,41 @@ pygame.display.set_caption("Spritesheets")
 
 clock = pygame.time.Clock()
 
+spritesheet = pygame.image.load("assets/Soldier/Soldier-Walk.png").convert_alpha()
+num_frames = 8
+
+sprite = Sprite(spritesheet, num_frames, SCREEN_WIDTH//2, SCREEN_HEIGHT//2, 0.1)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(sprite)
+
 run = True
 while run:
   #event handler
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
-     # update background
+
+  keys = pygame.key.get_pressed()
+  dx, dy = 0, 0
+
+  if keys[pygame.K_LEFT]:
+    dx = -5
+  if keys[pygame.K_RIGHT]:
+    dx = 5
+  if keys[pygame.K_UP]:
+    dy = -5
+  if keys[pygame.K_DOWN]:
+    dy = 5
+
+  sprite.move(dx, dy)
+     
+  all_sprites.update()
+
   screen.fill(CUSTOM_COLOR)
   
+  all_sprites.draw(screen)
+
   pygame.display.update()
 
   clock.tick(60)
--- sprite.py
+++ sprite.py
@@ -1,4 +1,33 @@
 import pygame
-class Sprite():
-  def __init__(self):
-    pass
+class Sprite(pygame.sprite.Sprite):
+  def __init__(self, spritesheet, num_frames, x, y, animation_speed):
+    super().__init__()
+    self.images = self.load_images_from_spritesheet(spritesheet, num_frames)
+    self.image = self.images[0]
+    self.rect = self.image.get_rect(center=(x, y))
+    self.animation_speed = animation_speed
+    self.current_time = 0
+    self.index = 0
+
+  def load_images_from_spritesheet(self, spritesheet, num_frames):
+    sheet_width = spritesheet.get_width()
+    sheet_height = spritesheet.get_height()
+    frame_width = sheet_width // num_frames
+
+    images = []
+    for i in range(num_frames):
+      frame = spritesheet.subsurface((i * frame_width, 0, frame_width, sheet_height))
+      images.append(frame)
+    return images
+
+  def update(self):
+      self.current_time += self.animation_speed
+      if self.current_time >= 1:
+        self.current_time = 0
+        self.index = (self.index + 1) % len(self.images)
+        self.image = self.images[self.index]
+
+  
+  def move(self, dx, dy):
+      self.rect.x += dx
+      self.rect.y += dy
